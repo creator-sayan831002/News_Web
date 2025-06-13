debd685f78922757734e48c461866879fd59f697
Sayan Das <sayandas9b35@gmail.com>
2025-06-13 18:00:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function reload() {
+    window.location.reload();
+}
+
+function onNavItemClick(category) {
+    alert('Category clicked: ' + category);
+}
 const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
 const url = "https://newsapi.org/v2/everything?q=";
 
